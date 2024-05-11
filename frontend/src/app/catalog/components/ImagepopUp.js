36c3487621fa5f398c7d7e30ce11f182b65730c1
salimKurbanov <salim.kurbanov08@gmail.com>
2024-05-11 23:35:56 +0300

    salik sdelal huyniu

--- frontend/src/app/catalog/components/ImagepopUp.js
+++ frontend/src/app/catalog/components/ImagepopUp.js
@@ -1,11 +1,23 @@
+'use client'
+
+import { useRef, useEffect } from "react";
 import PopUpMainSplider from "./PopUpMainSplider";
 import PopUpVerticalSplider from "./PopUpVerticalSplider";
 
-export default function ImagePopUp() {
+export default function ImagePopUp({Image1, Image2, Image3, Image4, Image5}) {
+
+    const main = useRef()
+    const vertical = useRef()
+
+    useEffect(() => {
+        main.current.sync(vertical.current.splide);
+      }, [main, vertical]);
+    
+
     return (
         <div className="image_popup">
-            <PopUpVerticalSplider />
-            <PopUpMainSplider />
+            <PopUpVerticalSplider vertical={vertical} Image1={Image1} Image2={Image2} Image3={Image3} Image4={Image4} Image5={Image5}/>
+            <PopUpMainSplider main={main} Image1={Image1} Image2={Image2} Image3={Image3} Image4={Image4} Image5={Image5}/>
         </div>
     );
 }
